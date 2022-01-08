4d146999fd7bc26e799f4890d742a821e20097f4
atonus <atonus@antonvalle.fi>
2022-01-08 19:35:55 +0200

    refactor tests to given when then

--- src/tests/test_endpoints.py
+++ src/tests/test_endpoints.py
@@ -1,16 +1,40 @@
-import os
 from .context import endpoints
 from fastapi.testclient import TestClient
 
 client = TestClient(endpoints.app)
 
-# def test_get_root():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json() == ["Hello world!"]
+
+def test_get_root():
+    # given
+    route = "/"
+
+    # when
+    response = client.get(route)
+
+    # then
+    assert response.status_code == 200
+    assert response.json() == ["Hello world!"]
 
 
-# def test_get_closest_asteroid():
-#     response = client.get("/show-closest-asteroid")
-#     assert response.status_code == 200
-#     assert response.json()
+def test_get_closest_asteroid():
+    # given
+    route = "/show-closest-asteroid"
+
+    # when
+    response = client.get(route)
+
+    # then
+    assert response.status_code == 200
+    assert response.json()
+
+
+def test_get_largest_asteroid():
+    # given
+    route = "/show-largest-asteroid"
+
+    # when
+    response = client.get(route)
+
+    # then
+    assert response.status_code == 200
+    assert response.json()
--- src/tests/test_nasa_api.py
+++ src/tests/test_nasa_api.py
@@ -1,8 +1,6 @@
-import os
 import json
 import unittest
 from .context import nasa_api
-import sys
 
 
 class TestNasaApi(unittest.TestCase):
@@ -13,24 +11,37 @@ class TestNasaApi(unittest.TestCase):
             data = json.load(file)
             return data
 
-    # def test_feed(self):
-    #     n = nasa_api.NasaApi()
-    #     result = n._feed()
-    #     self.assertEqual(str(list(result.keys())[0]), 'links')
-
     def test_get_closest(self):
+        # given
         n = nasa_api.NasaApi()
-        result = n.get_closest(self.get_temp_file()["near_earth_objects"])
+        temp_file = self.get_temp_file()["near_earth_objects"]
+
+        # when
+        result = n.get_closest(temp_file)
+
+        # then
         self.assertTrue(isinstance(result, dict))
 
     def test_remove_days_from_other_years(self):
+        # given
         n = nasa_api.NasaApi()
+        temp_file = self.get_temp_file()["near_earth_objects"]
+
+        # when
         result = n.remove_days_from_other_years(
-            2015, self.get_temp_file()["near_earth_objects"])
+            2015, temp_file)
+
+        # then
         self.assertFalse("2016-01-02" in result.keys())
         self.assertTrue("2015-12-25" in result.keys())
 
     def test_get_largest(self):
+        # given
         n = nasa_api.NasaApi()
-        result = n.get_largest(self.get_temp_file()["near_earth_objects"])
+        temp_file = self.get_temp_file()["near_earth_objects"]
+
+        # when
+        result = n.get_largest(temp_file)
+
+        # then
         self.assertTrue(isinstance(result, dict))
