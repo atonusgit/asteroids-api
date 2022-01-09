257b9d7303a72d4b27c85372099ef0bd0e9b4580
atonus <atonus@antonvalle.fi>
2022-01-09 21:30:46 +0200

    remove cache file as parameter

--- src/modules/cache.py
+++ src/modules/cache.py
@@ -5,21 +5,23 @@ import globals_vars
 
 
 class Cache:
-    def __init__(self):
-        globals_vars.cache = self.get_cache_file(
-            str(pathlib.Path(__file__).parent.resolve()) + "/../cache.json")
+    _CACHE_FILE = str(pathlib.Path(
+        __file__).parent.resolve()) + "/../cache.json"
 
-    def save_cache_file(self, data, filename=str(pathlib.Path(__file__).parent.resolve()) + "/../cache.json"):
-        with open(filename, 'w') as f:
+    def __init__(self):
+        globals_vars.cache = self.get_cache_file()
+
+    def save_cache_file(self, data):
+        with open(self._CACHE_FILE, 'w') as f:
             json.dump(data, f, indent=4)
 
-    def get_cache_file(self, filename):
+    def get_cache_file(self):
         try:
-            with open(filename, "r") as file_data:
+            with open(self._CACHE_FILE, "r") as file_data:
                 return json.load(file_data)
         except:
             return {}
 
-    def delete_cache_file(self, filename=str(pathlib.Path(__file__).parent.resolve()) + "/../cache.json"):
-        if os.path.exists(filename):
-            os.remove(filename)
+    def delete_cache_file(self):
+        if os.path.exists(self._CACHE_FILE):
+            os.remove(self._CACHE_FILE)
